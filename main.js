b832f3f6d4960a00bbb83c0da14222b54eeeb12b
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-04 20:07:55 -0600

    Hero section

---
+++ main.js
@@ -0,0 +1,15 @@
+/* typingEffect */
+
+var typingEffect = new Typed(".multiText",{
+    strings : ["Web Developer", "UX/UI Designer", "Graphic Designer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
+
+var typingEffect = new Typed(".multiText2",{
+    strings : ["I'm Web Developer and UI/UX Designer"],
+    loop : false,
+    typeSpeed : 120,
+})
