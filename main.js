768d683d7235596e136a8d1441718d2343b3a39c
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-06 01:43:14 -0600

    Responsive menu mobile

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/* barsMenu*/
+
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+var menu = document.querySelector(".navbarItems-mobile");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+    menu.classList.toggle("activenavbarItems-mobile")
+
+}
+
 /* typingEffect */
 
 var typingEffect = new Typed(".multiText",{
