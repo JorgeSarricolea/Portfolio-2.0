643fda0f992ad7bcc71d3e9d09d1c357c953a93c
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-06 18:22:02 -0600

    JQuery modifications

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-/* barsMenu*/
+$(document).ready(function() {
 
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+    /* barsMenu*/
 
-var line1__bars = document.querySelector(".line1__bars-menu");
-var line2__bars = document.querySelector(".line2__bars-menu");
-var line3__bars = document.querySelector(".line3__bars-menu");
-var menu = document.querySelector(".navbarItems-mobile");
+    $(".bars__menu").click(function() {
+        $(".line1__bars-menu").toggleClass("activeline1__bars-menu");
+        $(".line2__bars-menu").toggleClass("activeline2__bars-menu");
+        $(".line3__bars-menu").toggleClass("activeline3__bars-menu");
+        $(".navbarItems-mobile").toggleClass("activenavbarItems-mobile");
+    });
 
-function animateBars(){
-    line1__bars.classList.toggle("activeline1__bars-menu");
-    line2__bars.classList.toggle("activeline2__bars-menu");
-    line3__bars.classList.toggle("activeline3__bars-menu");
-    menu.classList.toggle("activenavbarItems-mobile")
+    /* typingEffect */
 
-}
+    var typingEffect = new Typed(".multiText",{
+        strings : ["Web Developer", "UX/UI Designer", "Graphic Designer"],
+        loop : true,
+        typeSpeed : 100,
+        backSpeed : 80,
+        backDelay : 1500
+    });
 
-/* typingEffect */
-
-var typingEffect = new Typed(".multiText",{
-    strings : ["Web Developer", "UX/UI Designer", "Graphic Designer"],
-    loop : true,
-    typeSpeed : 100,
-    backSpeed : 80,
-    backDelay : 1500
-})
-
-var typingEffect = new Typed(".multiText2",{
-    strings : ["I'm Web Developer and UI/UX Designer"],
-    loop : false,
-    typeSpeed : 120,
-})
+    var typingEffect2 = new Typed(".multiText2",{
+        strings : ["I'm Web Developer and UI/UX Designer"],
+        loop : false,
+        typeSpeed : 120,
+    });
+});
