ba655496012bceea35520473ea94f02dc6ad6444
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-18 14:21:19 -0600

    Portfolio page

---
+++ js/portfolio.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+        /* Buttons of the projects
+    ----------------*/
+
+    $('.btn-jumwork').click(function() {
+        window.open($(this).attr('href'));
+    });
+
+    $('.btn-calorie').click(function() {
+        window.open($(this).attr('href'));
+    });
+
+    $('.btn-recipies').click(function() {
+        window.open($(this).attr('href'));
+    });
+
+    $('.btn-joorge').click(function() {
+        window.open($(this).attr('href'));
+    });
+
+    $('.btn-weatherapp').click(function() {
+        window.open($(this).attr('href'));
+    });
+
+    $('.btn-spotifyclon').click(function() {
+        window.open($(this).attr('href'));
+    });
+});
