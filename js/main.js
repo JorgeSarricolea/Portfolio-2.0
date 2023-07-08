814acda943ecc7cc17a5837c9b03d94d3fd63618
JorgeSarricolea <jjorgesarricolea18@gmail.com>
2023-07-07 21:16:38 -0600

    style: Change profile pictures in Home and About Pages

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   });
 
   /* Multilanguaje swith
-    ----------------*/
+    ----------------
 
   const flagsElement = $("#flags");
   const textsToChange = $("[data-section]");
