1aa4e734370a2389ba2e6d1ba539ddbbb28bd7b6
JorgeSarricolea <jjorgesarricolea18@gmail.com>
2023-07-07 20:05:38 -0600

    feat: Add multilanguaje function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
 $(document).ready(function () {
-
-    /* Fucntion to send the user to a new tab
+  /* Fucntion to send the user to a new tab
     ----------------*/
 
-    $("#primary-btn").click(function () {
-        window.open($(this).attr("href"), "_blank");
-    });
+  $("#primary-btn").click(function () {
+    window.open($(this).attr("href"), "_blank");
+  });
 
-    $("#secondary-btn").click(function () {
-        window.open($(this).attr("href"), "_blank");
-    });
+  $("#secondary-btn").click(function () {
+    window.open($(this).attr("href"), "_blank");
+  });
 
-    /* Typing Effect
+  /* Multilanguaje swith
     ----------------*/
 
-    var typingEffect = new Typed(".multiText", {
-        strings: ["Web Developer", "UX/UI Designer", "Graphic Designer"],
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 80,
-        backDelay: 1500
+  const flagsElement = $("#flags");
+  const textsToChange = $("[data-section]");
+
+  const changeLanguaje = async (languaje) => {
+    const requestJson = await fetch(`./languajes/${languaje}.json`);
+    const texts = await requestJson.json();
+
+    console.log(texts);
+    textsToChange.each(function () {
+      const section = $(this).data("section");
+      const value = $(this).data("value");
+
+      $(this).html(texts[section][value]);
     });
+  };
+
+  flagsElement.on("click", (e) => {
+    changeLanguaje(e.target.parentElement.dataset.languaje);
+  });
+
+  /* Typing Effect
+    ----------------*/
+
+  var typingEffect = new Typed(".multiText", {
+    strings: ["Web Developer", "UX/UI Designer", "Graphic Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500,
+  });
 });
